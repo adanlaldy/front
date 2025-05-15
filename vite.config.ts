b6a972a0f9910713540584f5c8f475a675392c9c
nada <adan.laldymaquiha@ynov.com>
2025-05-15 15:04:50 +0200

    feat(dev): init the PWA react application

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+
+    manifest: {
+      name: 'front',
+      short_name: 'front',
+      description: 'front',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+  })],
+})
